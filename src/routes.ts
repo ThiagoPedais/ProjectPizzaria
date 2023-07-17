2ca18b169ecd7b5f060dac55e99320b17b3ae075
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-07-17 17:14:28 -0300

    finish order

--- src/routes.ts
+++ src/routes.ts
@@ -17,6 +17,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrderController } from './controllers/order/ListOrderController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 
 
@@ -45,6 +46,7 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
 
 
 export { router };
